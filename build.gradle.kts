e46b90e2c62f9beb06267cc05ddb5d56bd46b19e
arksu <ark@ark.su>
2023-07-31 02:24:54 +0500

    upd versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jooq.meta.jaxb.Logging
 
 plugins {
-    val kotlinVersion = "1.8.21" // https://kotlinlang.org/
-    val springVersion = "3.0.7" // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
+    val kotlinVersion = "1.8.22" // https://kotlinlang.org/
+    val springVersion = "3.1.2" // https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-web
 
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
     id("org.springframework.boot") version springVersion
-    id("io.spring.dependency-management") version "1.1.0" // https://plugins.gradle.org/plugin/io.spring.dependency-management
-    id("org.flywaydb.flyway") version "9.18.0" // https://plugins.gradle.org/plugin/org.flywaydb.flyway
+    id("io.spring.dependency-management") version "1.1.2" // https://plugins.gradle.org/plugin/io.spring.dependency-management
+    id("org.flywaydb.flyway") version "9.21.1" // https://plugins.gradle.org/plugin/org.flywaydb.flyway
     id("nu.studer.jooq") version "8.2.1" // https://plugins.gradle.org/plugin/nu.studer.jooq
 }
 
@@ -30,9 +30,9 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions:1.2.2")
-    implementation("org.flywaydb:flyway-core:9.18.0")
-    implementation("org.jooq:jooq-kotlin:3.18.4")
-    implementation("org.jooq:jooq-kotlin-coroutines:3.18.4")
+    implementation("org.flywaydb:flyway-core:9.21.1")
+    implementation("org.jooq:jooq-kotlin:3.18.5") // https://mvnrepository.com/artifact/org.jooq/jooq
+    implementation("org.jooq:jooq-kotlin-coroutines:3.18.5")
 
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
@@ -54,9 +54,9 @@ dependencies {
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
-    testImplementation("org.testcontainers:testcontainers:1.18.1") // https://mvnrepository.com/artifact/org.testcontainers/testcontainers
-    testImplementation("org.testcontainers:postgresql:1.18.1") // https://mvnrepository.com/artifact/org.testcontainers/testcontainers
-    testImplementation("org.testcontainers:junit-jupiter:1.18.1") // https://mvnrepository.com/artifact/org.testcontainers/testcontainers
+    testImplementation("org.testcontainers:testcontainers:1.18.3") // https://mvnrepository.com/artifact/org.testcontainers/testcontainers
+    testImplementation("org.testcontainers:postgresql:1.18.3") // https://mvnrepository.com/artifact/org.testcontainers/testcontainers
+    testImplementation("org.testcontainers:junit-jupiter:1.18.3") // https://mvnrepository.com/artifact/org.testcontainers/testcontainers
     testImplementation("com.github.javafaker:javafaker:1.0.2") // https://github.com/DiUS/java-faker
 }
 
@@ -85,7 +85,7 @@ flyway {
 }
 
 jooq {
-    version.set("3.18.4") // https://mvnrepository.com/artifact/org.jooq/jooq
+    version.set("3.18.5") // https://mvnrepository.com/artifact/org.jooq/jooq
     configurations {
         create("main") {  // name of the jOOQ configuration
             generateSchemaSourceOnCompilation.set(true)  // default (can be omitted)
