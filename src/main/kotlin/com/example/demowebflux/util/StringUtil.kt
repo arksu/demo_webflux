3c56454b30d44f53383e9378a9617ae62281e024
arksu <ark@ark.su>
2023-09-08 01:58:51 +0500

    secure random

--- src/main/kotlin/com/example/demowebflux/util/StringUtil.kt
+++ src/main/kotlin/com/example/demowebflux/util/StringUtil.kt
@@ -1,5 +1,6 @@
 package com.example.demowebflux.util
 
+import java.security.SecureRandom
 import kotlin.random.Random
 
 val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
@@ -9,6 +10,9 @@ fun randomStringByKotlinRandom(len: Int) = (1..len)
     .map { Random.nextInt(0, charPool.size).let { charPool[it] } }
     .joinToString("")
 
-fun randomHexStringByKotlinRandom(len: Int) = (1..len)
-    .map { Random.nextInt(0, charHexPool.size).let { charHexPool[it] } }
-    .joinToString("")
+fun randomHexStringByKotlinRandom(len: Int): String {
+    val r = SecureRandom()
+    return (1..len)
+        .map { r.nextInt(0, charHexPool.size).let { charHexPool[it] } }
+        .joinToString("")
+}
